6cb2a0c3bcd7620fe9f7ac38396fa066e207f171
Zona <>
2025-08-14 17:34:02 +0800

    添加多语言

--- Flutter/tencent_conference_uikit/lib/common/languages/translation_service.dart
+++ Flutter/tencent_conference_uikit/lib/common/languages/translation_service.dart
@@ -4,11 +4,33 @@ import 'package:get/get.dart';
 
 import 'en_us.dart';
 import 'zh_cn.dart';
+import 'de.dart';
+import 'es.dart';
+import 'fr.dart';
+import 'hi.dart';
+import 'it.dart';
+import 'ja.dart';
+import 'ko.dart';
+import 'mk.dart';
+import 'pt.dart';
+import 'sr.dart';
+import 'vi.dart';
 
 class RoomContentsTranslations {
   static const Map<String, Map<String, String>> translations = {
     'en': roomContentsEnUS,
     'zh': roomContentsZhCN,
+    'de': roomContentsDeDE,
+    'es': roomContentsEsES,
+    'fr': roomContentsFrFR,
+    'hi': roomContentsHiIN,
+    'it': roomContentsItIT,
+    'ja': roomContentsJaJP,
+    'ko': roomContentsKoKR,
+    'mk': roomContentsMkMK,
+    'pt': roomContentsPtPT,
+    'sr': roomContentsSr,
+    'vi': roomContentsVi,
   };
 
   static const String fallbackLocale = 'en';
@@ -20,6 +42,17 @@ class RoomContentsTranslations {
     switch (languageCode) {
       case 'zh':
       case 'en':
+      case 'de':
+      case 'es':
+      case 'fr':
+      case 'hi':
+      case 'it':
+      case 'ja':
+      case 'ko':
+      case 'mk':
+      case 'pt':
+      case 'sr':
+      case 'vi':
         Get.locale ??= deviceLocale;
         break;
       default:
